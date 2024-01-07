7daaa5eb1564246547b40513412a89202d13edca
Aliaksandr Zamirouski <alex.zamirouski@gmail.com>
2024-01-07 12:39:52 +0300

    bff logic added

--- bff-service/src/app.controller.ts
+++ bff-service/src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  All,
+  Controller,
+  Get,
+  Param,
+  Req,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get(['', 'ping'])
+  healthCheck(): any {
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'OK',
+    };
+  }
+
+  @All(['/:service', '/:service/*'])
+  async processRequest(
+    @Req() request: Request,
+    @Res() response: Response,
+    @Param('service') service: string,
+  ) {
+    const result = await this.appService.getResponse(service, request);
+
+    return response.set(result.headers).status(result.status).send(result.data);
   }
 }
